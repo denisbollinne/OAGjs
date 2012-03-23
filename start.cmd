94fe4978d716c6141482abd46ab46aed46414b92
Denis Bollinne <denis.bollinne@itense.be>
2012-03-23 21:35:57 +0100

    use Redis as session store (a lot faster... on paper :D)

--- start.cmd
+++ start.cmd
@@ -1,1 +1,2 @@
-.\mongo\bin\mongod.exe --dbpath .\mongo\data\db --rest
+start "window1" /min cmd.exe /c .\redis\redis-server.exe
+start "window2" /min cmd.exe /c .\mongo\bin\mongod.exe --dbpath .\mongo\data\db --rest
