cc4b256aa7b7b51ea64ac3d89f12efbe3016ad82
Denis Bollinne <denis.bollinne@itense.be>
2012-03-30 20:50:26 +0200

    redis for socket.io, investigating

--- init/redisFactory.js
+++ init/redisFactory.js
@@ -18,9 +18,21 @@ module.exports = function(express) {
     var RedisStore = require('connect-redis')(express);
 
     function ConnectHerokuRedis(options) {
-        var options2 = GetOptions(options);
+        var redisToGo = process.env.REDISTOGO_URL ? parse(process.env.REDISTOGO_URL) : false;
+        console.log("redisToGoURL", redisToGo);
+        options = options || {};
 
-        RedisStore.call(this, options2);
+        if (redisToGo) {
+            options.host = options.host || redisToGo.host;
+            options.port = options.port || redisToGo.port;
+
+            if (!options.pass && redisToGo.auth) {
+                options.pass = options.pass || redisToGo.auth.split(":")[1];
+            }
+        }
+
+
+        RedisStore.call(this, options);
     }
 
     // Inherit from Connect Redis
@@ -28,22 +40,6 @@ module.exports = function(express) {
 
 
     function CreateClient(options) {
-        options2 = GetOptions(options);
-
-        var rc =  new redis.createClient(options2.port || options2.socket, options2.host, options2)
-
-        rc.on('error', function(err){
-            console.log('RC ERROR : '+err);
-        });
-        rc.on('ready', function(){
-            console.log('this doesn\'t get printed');
-        });
-        return rc;
-    }
-
-
-
-    function GetOptions(options) {
         var redisToGo = process.env.REDISTOGO_URL ? parse(process.env.REDISTOGO_URL) : false;
         console.log("redisToGoURL", redisToGo);
         options = options || {};
@@ -56,8 +52,18 @@ module.exports = function(express) {
                 options.pass = options.pass || redisToGo.auth.split(":")[1];
             }
         }
-        return options;
+
+        var rc =  new redis.createClient(options.port || options.socket, options.host, options)
+
+        rc.on('error', function(err){
+            console.log('RC ERROR : '+err);
+        });
+        rc.on('ready', function(){
+            console.log('this doesn\'t get printed');
+        });
+        return rc;
     }
+
 
     return {CreateClient : CreateClient, CreateSessionStore: ConnectHerokuRedis};
 
