fa7334c37a805fbad60843db992014d089b2e98f
Denis Bollinne <denis.bollinne@itense.be>
2012-03-31 09:30:48 +0200

    removed the GET create API and add POST Create API

--- resources/characters.js
+++ resources/characters.js
@@ -15,12 +15,17 @@ exports.all = function(req, res){
 };
 
 exports.new = function(req, res){
+    //SEND new character html page
+   // res.send(jadePage);
+};
+
+exports.create = function(req, res){
     var newChar = new character();
     newChar.user = req.user;
-    newChar.name = req.user._id;
+    newChar.name = req.body.name
     newChar.experience = 0;
-    newChar.class = 'Barbarian';
-    newChar.race = 'Skeletton';
+    newChar.class = req.body.class;
+    newChar.race = req.body.race;
 
     var charPos = new position();
     charPos.x = 150;
@@ -34,11 +39,8 @@ exports.new = function(req, res){
     newChar.save();
     charPos.save();
 
-    res.send(newChar);
-};
-
-exports.create = function(req, res){
-    res.send('create character');
+   //This should probably return a jade page with the newly created char
+   // res.send(newChar);
 };
 
 exports.show = function(req, res){
@@ -52,20 +54,21 @@ exports.edit = function(req, res){
 exports.update = function(req, res){
     res.send('update character ' + req.params.id);
 };
-
+//SHOULD BE A POST
 exports.destroy = function(req, res){
     character.findOne({user:req.user._id, _id:req.param.id}).populate('position').run(function(err,docs){
         docs.remove();
         res.send('destroy character ' + req.params.id);
     });
 };
-
+//SHOULD BE A POST
 exports.destroyAll = function(req, res){
     character.remove({user:req.user._id}, function(err,docs){
         res.send('destroy all characters');
     });
 };
 
+//SHOULD BE A POST
 exports.select = function(req,res){
     character.findOne({user:req.user._id, name:req.params.name},function(err,docs){
         if(docs != null){
