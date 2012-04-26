e29e58a90e44c462200ed832353c41824ea0821d
Maarten De Wilde <maarten.dewilde@gmail.com>
2012-04-26 20:21:09 +0200

    Remove animations

--- public/js/player.js
+++ public/js/player.js
@@ -10,7 +10,7 @@ GAME.Player = function (gs,playable, currentPlayerInfo,hammer){
     var animations = new GAME.AnimationFactory();
 
 
-    var knightAnimations = new GAME.Animations();
+    var knightAnimations = {};
     var tileset = "/img/knight/tileSet.png";
     knightAnimations.runWest = new GAMEFW.AnimDef(tileset,2,12,276);
     knightAnimations.runEast = new GAMEFW.AnimDef(tileset,2,12,192);
