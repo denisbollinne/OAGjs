9396a80b400fd2afcc3e3a6dd98b58c3a091c5f7
Denis Bollinne <denis.bollinne@itense.be>
2012-04-28 12:52:30 +0200

    display image in canvas

--- public/js/jade/createArena.js
+++ public/js/jade/createArena.js
@@ -1,6 +1,43 @@
-var Arena = {};
-Arena.CreateArena = function(){
+$('#fileSelect').change(function (evt) {
+    new Arena.CreateArena(evt.target);
+});
 
-    var gs = new JSGameSoup("surface", GAME.framerate);
+var Arena = {};
+Arena.CreateArena = function (imageList) {
+
+    var file = imageList.files[0];
+
+    var gs = new JSGameSoup("surface", 20);
+
+    var reader = new FileReader();
+    reader.onload = (function(theFile) {
+        return function(e) {
+            var img = new Image();
+            img.onload = function(){
+                var imageEntity = new Arena.Image(img);
+                gs.addEntity(imageEntity);
+                gs.launch();
+            };
+            img.src =  e.target.result;
+        };
+    })(file);
+
+    reader.readAsDataURL(file);
+
+
+
+
+
+}
+
+Arena.Image = function (imageSource) {
+    var image = imageSource;
+    //    this.update = function (){
+    //
+    //    };
+
+    this.draw = function (c) {
+        c.drawImage(image,0,0);
+    };
 }
 
