7ead6781d4ab3d9fc110aa28e1d684d01e6cdac5
Denis Bollinne <denis.bollinne@itense.be>
2012-08-19 10:29:54 +0200

    update code to work with Express 3

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 define(['express','init/bootstrap','mongoose'],function(express,bootstrapper,mongoose){
-    var app =express.createServer();
+    var app =express();
     bootstrapper(app);
     User = mongoose.model('User');
     return app;
